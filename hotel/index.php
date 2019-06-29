da7e79c74e172651e62f84ac234c6083948ede6b
utidanet <utidanet>
2019-06-30 01:45:23 +0900

    wordpressのタグを挿入中

--- hotel/index.php
+++ hotel/index.php
@@ -3,50 +3,17 @@
     <meta charset="UTF-8">
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/styles.css" type="text/css" />
 <!--[if lt IE 9]>
-	
-	
-	
-	
-	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/styles.css" type="text/css" />
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	<script src="https://html5shiv.goo
+<script src="https://html5shiv.goo
 glecode.com/svn/trunk/html5.js"></script>
 <script src="https://ie7-js.googlecode.com/svn/version/2.1(beta4)/IE9.js"></script>
 <![endif]-->
-<title>サンプルサイト</title>
+<title><?php bloginfo('name'); ?></title>
 </head>
 <body class="home">
     <header class="globalHeader">
         <div class="inner">
             <h1><a href="/"><img src="<?php echo get_template_directory_uri(); ?>/images/common/logo01.png" height="40" width="300" alt="ホテル・技評リゾート石垣島"></a></h1>
-            <p class="description">サイトのキャッチフレーズ</p>
+            <p class="description"><?php bloginfo('description'); ?></p>
             <form action="" method="get" class="searchform">
                 <input type="text" value="" name="s" id="s" placeholder="サイト内検索">
                 <input type="submit" id="searchSubmit" value="">
