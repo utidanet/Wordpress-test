ff9d3da80e8c34809d773437b93de290e89bf9dd
utidanet <utidanet>
2019-07-01 01:00:13 +0900

    2019年7月1日　0時59分　パーツごとに分離

--- hotel/index.php
+++ hotel/index.php
@@ -1,25 +1,4 @@
-<!DOCTYPE html>
-<html lang="ja"><head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/styles.css" type="text/css" />
-<!--[if lt IE 9]>
-<script src="https://html5shiv.goo
-glecode.com/svn/trunk/html5.js"></script>
-<script src="https://ie7-js.googlecode.com/svn/version/2.1(beta4)/IE9.js"></script>
-<![endif]-->
-<title><?php bloginfo('name'); ?></title>
-</head>
-<body class="home">
-    <header class="globalHeader">
-        <div class="inner">
-            <h1><a href="/"><img src="<?php echo get_template_directory_uri(); ?>/images/common/logo01.png" height="40" width="300" alt="ホテル・技評リゾート石垣島"></a></h1>
-            <p class="description"><?php bloginfo('description'); ?></p>
-            <form action="" method="get" class="searchform">
-                <input type="text" value="" name="s" id="s" placeholder="サイト内検索">
-                <input type="submit" id="searchSubmit" value="">
-            </form>
-        </div>
-    </header><!-- /.globalHeader -->
+<?php get_header() ;?>
 
     <div class="homeVisual"><span>石垣島でのんびりゆったりと。</span></div>
 
@@ -112,36 +91,7 @@ glecode.com/svn/trunk/html5.js"></script>
         </div><!-- /.mainContents -->
 
         <aside class="subContents">
-            <p class="bnr">
-                <a href="../html/contact.html"><img src="<?php echo get_template_directory_uri(); ?>/images/common/bnr_contact01.png" height="183" width="300" alt="宿泊のお問い合わせ"></a>
-            </p>
-            <div class="wrapper">
-                <section class="socialBanners">
-                    <h2 class="hidden">関連バナー</h2>
-                    <div class="banners">
-                        <ul>
-                            <li><a href="" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/common/bnr_fb01.png" height="60" width="290" alt=""></a></li>
-                            <li><a href="" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/common/bnr_tw01.png" height="60" width="290" alt=""></a></li>
-                            <li><a href="" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/common/bnr_rss01.png" height="60" width="290" alt=""></a></li>
-                        </ul>
-                    </div>
-                </section><!-- /.socialBanners -->
-            </div><!-- /.wrapper -->
+<?php get_sidebar(); ?>
         </aside><!-- /.subContents -->
     </div><!-- /.contentsWrap -->
-
-    <footer class="globalFooter">
-        <div class="pageTop">
-            <p><a href="javascript:void(0);" id="js-pagetop"><img src="<?php echo get_template_directory_uri(); ?>/images/common/pagetop01.png" height="41" width="41" alt=""></a></p>
-        </div>  
-        <div class="inner">
-            <dl class="address">
-                <dt>ホテル・技評リゾート石垣島</dt>
-                <dd>〒123-4567 沖縄県石垣市0123</dd>
-                <dd>TEL 0123(45)6789 / FAX 0123(45)6789</dd>
-            </dl><!-- /.address -->
-            <small>&copy; HOTEL GIHYO RESORT ISHIGAKI All Rights Reserved.</small>
-        </div>
-    </footer><!-- /.globalFooter -->
-</body>
-</html>
+<?php get_footer(); ?>
