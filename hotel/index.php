ae07e879a7c67dd3372eb94893eda4fab62c880b
utidanet <utidanet>
2019-07-06 03:57:34 +0900

    wordpressループを制作　P077

--- hotel/index.php
+++ hotel/index.php
@@ -15,30 +15,30 @@
 
             <section class="newsBlock block">
                 <h1 class="type-B"><span>新着情報</span></h1>
-
-                
-                <?php if (have_posts() : 
+                <?php 
+                if ( have_posts() ) : 
                           while ( have_posts() ) : the_post(); 
                 ?>
                 <article id="post-<?php the_ID(); ?>" <?php post_class('news'); ?>>
                     <div class="text">
                         <div class="entryInfo">
                             <div class="categories">
-                                <?php the_categry(); ?>
+                                <?php the_category(); ?>
                             </div>
-                            <time datetime="<?php the_time('Y-m-d'); ?>"><?php the_time('Y年m月d日(1)'); ?></time>
+                            <time datetime="<?php the_time('Y-m-d'); ?>"><?php the_time('Y年m月d日(l)'); ?></time>
                         </div>
                         <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
                         <?php the_excerpt(); ?>
                         <p>[<a href="<?php the_permalink(); ?>">続きを読む</a>]</p>
                     </div>
                     <figure>
-                        <?php if (has_post_thumbnail() ): ?>
-                        <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('thumbnail'); ?></a>
-                         <?php else: ?>
-                        <a href="<?php the_permaiink(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/images/common/noimage_180*180.png" height"180" width="180" alt=""></a>
-                        <?php endif; ?>
+                       <?php if ( has_post_thumbnail() ): ?>
+                        <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('thumbnail'); ?>
+                            <?php else: ?>
+                            <a href="<?php the_permalink(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/images/common/noimage_180x180.png" height="180" width="180" alt=""></a>
+                            <?php endif; ?>
                     </figure>
+
                 </article><!-- /.news -->
                 <?php 
                           endwhile; 
